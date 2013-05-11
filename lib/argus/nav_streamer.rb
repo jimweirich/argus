73b2818355067918a95f64fcb189844987e517e8
Jim Weirich <jim.weirich@gmail.com>
2013-05-11 12:59:27 +0100

    Remove UdpNavStreamer

--- lib/argus/nav_streamer.rb
+++ lib/argus/nav_streamer.rb
@@ -1,18 +1,25 @@
 require 'socket'
 
 module Argus
-  class NavStreamer 
+  class NavStreamer
     def initialize(socket=nil, host='192.168.1.1', port='5554')
       @socket = socket || UDPSocket.new
-      @streamer = Argus::UdpNavStreamer.new(@socket, host, port)
+      @host = host
+      @port = port
+      @socket.bind("0.0.0.0", port)
     end
 
     def start
-      @streamer.start_stream
+      @socket.send("\x01\x00\x00\x00", 0, @host, @port)
     end
 
     def receive_data
-      @streamer.receive_packet
+      data, from = @socket.recvfrom(1024)
+      if data.unpack("V").first == 0x55667788
+        NavData.new(data)
+      else
+        nil
+      end
     end
   end
 end
