3acff85cc716dc9615843aca9229019804b00155
Jim Weirich <jim.weirich@gmail.com>
2013-05-30 12:58:26 -0400

    Add time queue

---
+++ lib/argus/time_queue.rb
@@ -0,0 +1,60 @@
+class TimeQueue
+  Item = Struct.new(:time, :value) do
+    def <=>(other)
+      time <=> other.time
+    end
+
+    def key
+      object_id
+    end
+
+    def to_s(basetime=nil)
+      if basetime
+        "#{value.inspect}@+#{time-basetime}"
+      else
+        "#{value.inspect}@#{time.strftime('%H:%M:%S.%L')}"
+      end
+    end
+  end
+
+  def initialize
+    @items = []
+  end
+
+  def add(time, value)
+    item = Item.new(time, value)
+    @items << item
+    @items.sort!
+    item.key
+  end
+
+  def remove(key)
+    @items.delete_if { |item| item.key == key }
+  end
+
+  def any_ready?(time)
+    ! @items.empty? && @items.first.time <= time
+  end
+
+  def all_ready(time)
+    result = []
+    each_ready(time) do |value| result << value end
+    result
+  end
+
+  def each_ready(time)
+    while any_ready?(time)
+      item = @items.shift
+      yield item.value
+    end
+  end
+
+  def to_s
+    base_time = nil
+    strings = @items[0,3].map { |item|
+      item.to_s(base_time).tap { base_time ||= item.time }
+    }
+    strings << "..." if @items.size > 3
+    "[#{strings.join(', ')}]"
+  end
+end
