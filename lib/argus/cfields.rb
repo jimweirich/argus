ee8d8677da4ab7677cc7df897a807a4d93b75feb
Jim Weirich <jim.weirich@gmail.com>
2013-08-17 05:10:05 -0400

    Streamline floating point decode of binary data.

--- lib/argus/cfields.rb
+++ lib/argus/cfields.rb
@@ -78,7 +78,7 @@ module Argus
       end
 
       def float32_t(name, size=nil)
-        define_field(name, size, "V") { |v| FloatEncoding.decode_float(v) }
+        define_field(name, size, "g")
       end
 
       def int32_t(name, size=nil)
