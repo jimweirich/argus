df5423812987b310f8e49e727807a6143af81ab4
Jim Weirich <jim.weirich@gmail.com>
2013-08-17 05:08:34 -0400

    add land rake command.

--- examples/land.rb
+++ examples/land.rb
@@ -1,16 +1,12 @@
 require 'argus'
 
-drone = Argus::Drone.new(remote_host: '192.168.1.200')
+drone = Argus::Drone.new
 drone.start
 
-puts "Hovering ..."
-10.times do
-  drone.hover
-end
-
 puts "Landing ..."
-10.times do
+5.times do
   drone.land
+  sleep 0.1
 end
 
 sleep 5
